c6fa9a21707962c1ab10411a0ace2c7357232daa
Evan Flynn <evanflynn.msu@gmail.com>
2024-03-21 06:24:37 -0700

    Add page for basketball boxscore
    Signed-off-by: Evan Flynn <evanflynn.msu@gmail.com>

--- components/pages/game.rs
+++ components/pages/game.rs
@@ -29,35 +29,95 @@ pub fn GamePage(props: &GameProps) -> Html {
         })});
     }
 
-    let header: Vec<_> = this_game.meta.teams.iter().map(|team| {
+    let mut team_stats: Vec<_> = this_game.meta.teams.iter().map(|team| {
         let Some(ref teams) = this_game.teams else { todo!() };
         let boxscore = if team.id == teams[0].id.to_string() {
             &teams[0]
         } else {
             &teams[1]
         };
-        // Determine which team this is (home or away)
-        let home_or_away: String = if
-            team.home_team.is_string() &&
-            team.home_team.as_str().unwrap().to_string() == "true"
-        {
-            "home".to_string()
-        } else {
-            "away".to_string()
-        };
+
+        let player_stats: Vec<_> = boxscore.player_stats.iter().map(|player| {
+            html! {
+                <tr>
+                    <td>{player.first_name.clone()}</td>
+                    <td>{player.last_name.clone()}</td>
+                    <th>{player.position.clone()}</th>
+                    <th>{player.points.clone()}</th>
+                    <th>{player.field_goals_made.clone()}</th>
+                    <th>{player.three_points_made.clone()}</th>
+                    <th>{player.free_throws_made.clone()}</th>
+                    <th>{player.total_rebounds.clone()}</th>
+                    <th>{player.offensive_rebounds.clone()}</th>
+                    <th>{player.assists.clone()}</th>
+                    <th>{player.personal_fouls.clone()}</th>
+                    <th>{player.steals.clone()}</th>
+                    <th>{player.turnovers.clone()}</th>
+                    <th>{player.blocked_shots.clone()}</th>
+                    <th>{player.minutes_played.clone()}</th>
+                </tr>
+        }}).collect();
 
         html! {
-            <div class={home_or_away}>
-                <h3>{team.short_name.clone()}</h3>
-                <p>{team.home_team.is_string()}</p>
+            <div>
+                <h3 class="game-team-name">{team.short_name.clone()}</h3>
+                <table class="styled-table">
+                    <thead>
+                        <tr>
+                            <th>{"FIRST"}</th>
+                            <th>{"LAST"}</th>
+                            <th>{boxscore.player_header.position.clone()}</th>
+                            <th>{boxscore.player_header.points.clone()}</th>
+                            <th>{boxscore.player_header.field_goals_made.clone()}</th>
+                            <th>{boxscore.player_header.three_points_made.clone()}</th>
+                            <th>{boxscore.player_header.free_throws_made.clone()}</th>
+                            <th>{boxscore.player_header.total_rebounds.clone()}</th>
+                            <th>{boxscore.player_header.offensive_rebounds.clone()}</th>
+                            <th>{boxscore.player_header.assists.clone()}</th>
+                            <th>{boxscore.player_header.personal_fouls.clone()}</th>
+                            <th>{boxscore.player_header.steals.clone()}</th>
+                            <th>{boxscore.player_header.turnovers.clone()}</th>
+                            <th>{boxscore.player_header.blocked_shots.clone()}</th>
+                            <th>{boxscore.player_header.minutes_played.clone()}</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {player_stats}
+                        <tr>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                            <td>{boxscore.player_totals.points.clone()}</td>
+                            <th>{boxscore.player_totals.field_goals_made.clone()}</th>
+                            <th>{boxscore.player_totals.three_points_made.clone()}</th>
+                            <th>{boxscore.player_totals.free_throws_made.clone()}</th>
+                            <th>{boxscore.player_totals.total_rebounds.clone()}</th>
+                            <th>{boxscore.player_totals.offensive_rebounds.clone()}</th>
+                            <th>{boxscore.player_totals.assists.clone()}</th>
+                            <th>{boxscore.player_totals.personal_fouls.clone()}</th>
+                            <th>{boxscore.player_totals.steals.clone()}</th>
+                            <th>{boxscore.player_totals.turnovers.clone()}</th>
+                            <th>{boxscore.player_totals.blocked_shots.clone()}</th>
+                        </tr>
+                    </tbody>
+                </table>
             </div>
         }
     }).collect();
 
+    if team_stats.is_empty() {
+        team_stats.push(html! {
+                <div>
+                  <h3>{"No boxscore available yet for this game"}</h3>
+                </div>
+        });
+    };
     html! {
         <div>
             <div class="game-header">
-                {header}
+            </div>
+            <div class="game-team-stats">
+                {team_stats}
             </div>
         </div>
     }
