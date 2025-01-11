bcefffc777073bb7794129d1e3f0ec97cb89d6f5
Evan Flynn <evanflynn.msu@gmail.com>
2025-01-10 19:28:01 -0800

    Remove extra clone
    Signed-off-by: Evan Flynn <evanflynn.msu@gmail.com>

--- components/cards/game.rs
+++ components/cards/game.rs
@@ -63,7 +63,7 @@ where
                     <h4 class="scoreboard-team-score home">{props.game.details.home.score.as_str().clone()}</h4>
                     <div class="scoreboard-team-details">
                       <h5 class="scoreboard-team-name home">
-                        <p class="scoreboard-team-rank home">{props.game.details.home.rank.as_str().clone()}</p>
+                        <p class="scoreboard-team-rank home">{props.game.details.home.rank.as_str()}</p>
                         {props.game.details.home.names.char6.clone()}
                       </h5>
                       <p class="scoreboard-team-record home">{props.game.details.home.description.clone()}</p>
@@ -75,7 +75,7 @@ where
                     <div class="scoreboard-team-details">
                       <h5 class="scoreboard-team-name">
                         {props.game.details.away.names.char6.clone()}
-                        <p class="scoreboard-team-rank away">{props.game.details.away.rank.as_str().clone()}</p>
+                        <p class="scoreboard-team-rank away">{props.game.details.away.rank.as_str()}</p>
                       </h5>
                       <p class="scoreboard-team-record away">{props.game.details.away.description.clone()}</p>
                     </div>
